985f8a84f83d17a1ba6869873c8a7fce48f68a8f
ziad shaarawy <ziadsayed2023@gmail.com>
2021-04-12 20:26:42 +0200

    student structure

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct birth
+{
+    int year;
+    int month;
+    int day;
+};
+
+struct student
+{
+    char name[100];
+    int id;
+    birth year;
+    int score;
+};
+
+int main ()
+{
+
+}
